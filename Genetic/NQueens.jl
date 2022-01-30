d6d08730e4cbf096c02e52dd3d7e569fc67b067e
MahanBi <DirtyMBGamer@gmail.com>
2022-01-30 17:16:27 +0330

    start Genetic with NQueen

---
+++ Genetic/NQueens.jl
@@ -0,0 +1,24 @@
+N::Int8 = 0;
+const InitMake::Int16, MutRate::Float16 = 200 , 0.8;
+
+function makelists(initsize::Int, N::Int8)::Array
+    all_board::Array = []
+    for i = 1:N
+        one_board::Array = []
+        
+    end
+end
+
+function crossover()::Array
+    
+end
+
+function mutation()::Array
+    
+end
+
+function fitness()::Array
+    
+end
+
+
